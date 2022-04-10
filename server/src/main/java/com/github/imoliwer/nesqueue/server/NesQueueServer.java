d263e68a1cd6be62381642667fbb6d86c1f62693
imoliwer <oliwer.lindell@hotmail.com>
2022-04-10 19:08:53 +0200

    Removal and addition of relations is now implemented (not tested).

--- server/src/main/java/com/github/imoliwer/nesqueue/server/NesQueueServer.java
+++ server/src/main/java/com/github/imoliwer/nesqueue/server/NesQueueServer.java
@@ -1,21 +1,79 @@
 package com.github.imoliwer.nesqueue.server;
 
+import com.github.imoliwer.nesqueue.server.connection.Options;
 import com.github.imoliwer.nesqueue.shared.Converter;
+import com.github.imoliwer.nesqueue.shared.base.NesQueueBase;
+import com.github.imoliwer.nesqueue.shared.crypto.CryptoHandle;
+import com.github.imoliwer.nesqueue.shared.timer.TimerFactory;
 
-public interface NesQueueServer<Key, Relation, Data> {
-    void enter(Key queue, Relation key, Comparable<Data> data);
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
-    void leave(Key queue, Relation key);
+/**
+ * This interface represents the NesQueue server.
+ *
+ * @param <Relation> type of queue-to-client relation to store in each queue.
+ * @param <Data>     type of data to store and make use of in update notifications.
+ */
+public interface NesQueueServer<Relation, Data> extends NesQueueBase {
+    /** {@link Byte} id of the 'successful' state. **/
+    byte SUCCESSFUL            = 0x00;
 
-    void next(Key queue);
+    /** {@link Byte} id of the 'queue index is not existent' state. **/
+    byte INVALID_QUEUE         = 0x01;
 
-    void start();
+    /** {@link Byte} id of the 'relation is already in the same queue' state. **/
+    byte ALREADY_IN_SAME_QUEUE = 0x02;
 
-    void setKeyAdapter(Converter<Key, String> adapter);
+    /** {@link Byte} id of the 'relation is not in a queue' state. **/
+    byte NOT_IN_QUEUE          = 0x03;
 
+    /**
+     * Add a new relation with data to queue by its key.
+     *
+     * @param queueIndex {@link Integer} the queue to add said relation to.
+     * @param key        {@link Relation} the key to be added.
+     * @param data       {@link Data} the data to be linked.
+     * @return {@link Byte} result of this addition.
+     */
+    byte add(int queueIndex, Relation key, Data data);
+
+    /**
+     * Remove a relation from queue by key.
+     *
+     * @param key {@link Relation} the key of said relation to remove.
+     * @return {@link Byte} result of this removal.
+     */
+    byte remove(Relation key);
+
+    /**
+     * Handle the next relation in queue by key.
+     *
+     * @param queueIndex {@link Integer} the queue to perform next op on.
+     */
+    void next(int queueIndex);
+
+    /**
+     * Set the converter for relational keys.
+     *
+     * @param adapter {@link Converter} the converter to be assigned.
+     */
     void setRelationAdapter(Converter<Relation, String> adapter);
 
-    static <K, R, D> NesQueueServer<K, R, D> create() {
-        return new ServerImpl<>();
+    /**
+     * Create a new NesQueue server.
+     *
+     * @param <R> the relational key type.
+     * @param <D> the data key type.
+     * @return {@link NesQueueServer} new instance of a NesQueue server.
+     */
+    static <R, D extends Comparable<D>> NesQueueServer<R, D> create(
+        InetSocketAddress address,
+        TimerFactory timerFactory,
+        CryptoHandle cryptoHandle,
+        Options options,
+        int queueCapacity
+    ) throws IOException {
+        return new ServerImpl<>(address, timerFactory, cryptoHandle, options, queueCapacity);
     }
 }
--- server/src/main/java/com/github/imoliwer/nesqueue/server/ServerImpl.java
+++ server/src/main/java/com/github/imoliwer/nesqueue/server/ServerImpl.java
@@ -1,37 +1,138 @@
 package com.github.imoliwer.nesqueue.server;
 
+import com.github.imoliwer.nesqueue.server.connection.Options;
+import com.github.imoliwer.nesqueue.server.connection.SocketServer;
 import com.github.imoliwer.nesqueue.server.exception.ConverterReassignException;
 import com.github.imoliwer.nesqueue.shared.Converter;
+import com.github.imoliwer.nesqueue.shared.crypto.CryptoHandle;
+import com.github.imoliwer.nesqueue.shared.timer.TimerFactory;
 
-final class ServerImpl<Key, Relation, Data> implements NesQueueServer<Key, Relation, Data> {
-    private Converter<Key, String> keyAdapter;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.InetSocketAddress;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Stream.generate;
+
+final class ServerImpl<Relation, Data extends Comparable<Data>> implements NesQueueServer<Relation, Data> {
+    static final class RelativeData<Relation, Data extends Comparable<Data>> implements Comparable<Data> {
+        final Relation relation;
+        final Data data;
+
+        RelativeData(Relation relation, Data data) {
+            this.relation = relation;
+            this.data = data;
+        }
+
+        @Override
+        public int compareTo(Data o) {
+            return data.compareTo(o);
+        }
+    }
+
+    static final class Notification implements Serializable {
+        final String type;
+        final int queueIndex;
+        final Object relation;
+        final Object data;
+
+        public Notification(String type, Object relation, int queueIndex, Object data) {
+            this.type = type;
+            this.relation = relation;
+            this.queueIndex = queueIndex;
+            this.data = data;
+        }
+    }
+
+    // handling
+    private final SocketServer server;
     private Converter<Relation, String> relationAdapter;
 
+    // storage
+    private final Map<Relation, Integer> relationQueueIndexes;
+    private final List<PriorityQueue<RelativeData<Relation, Data>>> queues;
+
+    public ServerImpl(
+        InetSocketAddress address,
+        TimerFactory timerFactory,
+        CryptoHandle cryptoHandle,
+        Options options,
+        int capacity
+    ) throws IOException {
+        this.server = SocketServer.create(
+            address,
+            timerFactory,
+            cryptoHandle,
+            options
+        );
+        this.relationQueueIndexes = new HashMap<>();
+        this.queues = generate(() -> new PriorityQueue<RelativeData<Relation, Data>>())
+            .limit(capacity)
+            .collect(Collectors.toUnmodifiableList());
+    }
+
     @Override
-    public void enter(Key queue, Relation key, Comparable<Data> data) {
+    public byte add(int queueIndex, Relation key, Data data) {
+        if (!this.checkLength(queueIndex)) {
+            server.broadcast(new Notification("join-invalid-queue", key, queueIndex, data));
+            return INVALID_QUEUE;
+        }
+
+        final var oldQueueIndex = relationQueueIndexes.put(key, queueIndex);
+        if (oldQueueIndex != null) {
+            if (oldQueueIndex == queueIndex) {
+                server.broadcast(new Notification("join-already-in-same-queue", key, queueIndex, data));
+                return ALREADY_IN_SAME_QUEUE;
+            }
+
+            final var oldQueue = queues.get(oldQueueIndex);
+            oldQueue.removeIf(it -> it.relation == key);
+        }
+
+        final var queue = queues.get(queueIndex);
+        queue.add(new RelativeData<>(key, data));
+        server.broadcast(new Notification("join-successful", key, queueIndex, data));
+        return SUCCESSFUL;
+    }
+
+    @Override
+    public byte remove(Relation key) {
+        final var queueIndex = relationQueueIndexes.remove(key);
+        if (queueIndex == null) {
+            server.broadcast(new Notification("leave-not-in-queue", key, -1, null));
+            return NOT_IN_QUEUE;
+        }
+
+        final var queue = queues.get(queueIndex);
+        final var relativeDataAtomic = new AtomicReference<RelativeData<Relation, Data>>();
+
+        queue.removeIf(it -> {
+            final var isAuthor = it.relation == key;
+            if (isAuthor) {
+                relativeDataAtomic.set(it);
+            }
+            return isAuthor;
+        });
+
+        server.broadcast(new Notification("leave-successful", key, queueIndex, relativeDataAtomic.get()));
+        return SUCCESSFUL;
+    }
+
+    @Override
+    public void next(int queueIndex) {
 
     }
 
     @Override
-    public void leave(Key queue, Relation key) {
-
+    public boolean start() {
+        return server.start();
     }
 
     @Override
-    public void next(Key queue) {
-
-    }
-
-    @Override
-    public void start() {
-
-    }
-
-    @Override
-    public void setKeyAdapter(Converter<Key, String> adapter) {
-        if (keyAdapter != null)
-            throw new ConverterReassignException("key");
-        this.keyAdapter = adapter;
+    public void stop() {
+        server.stop();
     }
 
     @Override
@@ -39,5 +140,10 @@ final class ServerImpl<Key, Relation, Data> implements NesQueueServer<Key, Relat
         if (relationAdapter != null)
             throw new ConverterReassignException("relation");
         this.relationAdapter = converter;
+    }
+
+    private boolean checkLength(int index) {
+        final var length = queues.size();
+        return length > index && index >= 0;
     }
 }
